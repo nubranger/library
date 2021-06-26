ccaf848e29a1f186ac9b750f2460e613549f84a7
nubranger <56818312+nubranger@users.noreply.github.com>
2021-06-26 12:58:56 +0300

    permission, alert updates

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,18 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+$('#summernote').summernote({
+    placeholder: 'About book',
+    tabsize: 2,
+    height: 120,
+    toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'underline', 'clear']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['fullscreen', 'codeview', 'help']]
+    ]
+});
